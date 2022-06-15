ce22789a0d232750a1128d7ddbfcc42fc45318c2
Niks Hariman <nhariman@hotmail.com>
2022-06-15 18:34:17 +0200

    forgot to save changes in vector

--- inc/ft_vector.hpp
+++ inc/ft_vector.hpp
@@ -6,7 +6,7 @@
 /*   By: nhariman <nhariman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/10 14:18:07 by nhariman      #+#    #+#                 */
-/*   Updated: 2022/06/15 16:41:05 by nhariman      ########   odam.nl         */
+/*   Updated: 2022/06/15 18:33:53 by nhariman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,20 @@ namespace ft
 			typedef allocator_type::pointer pointer;
 			typedef allocator_type::const_pointer const_pointer;
 			// iterators here????
-			struct Iterator
+			struct iterator
 			{
 				struct	input_iterator_tag {};
 				struct	output_iterator_tag {};
 				struct	forward_iterator_tag : public input_iterator_tag {};
 				struct	bidirectional_iterator_tag : public forward_iterator_tag {};
 				struct	random_access_iterator_tag : public	bidirectional_iterator_tag {};
+				// constructor
 				Iterator(pointer ptr) : m_ptr(ptr) {};
+				reference operator*() const
+				{
+					return *m_ptr;
+				}
+				pointer
 				private:
 					pointer m_ptr;
 			};
